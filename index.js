ac7ab02ce686d7bd7f549f89634d4faa13aef8d8
Joyce Lee <joycelee11@gmail.com>
2018-01-15 15:28:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function sayHiToGrandma(string) {
   } else if (string === uppercase) {
     return "YES INDEED!"
   } else if (string === I love you, Grandma.) {
-    return 
+    return "I love you, too."
   }
 }
